fc58b8fc498a36874d848c7793be9eb3a2d35c63
LiveCD Tester <coolo@suse.de>
2009-12-16 13:50:04 +0100

    initial commit

---
+++ gather_preload.sh
@@ -0,0 +1,105 @@
+set -e
+
+mkiso () 
+{
+  echo "mkiso"
+  cd CD1
+  boot=$(ls -1d boot/* | grep -v grub)
+  /usr/bin/mkisofs -R -J -pad -joliet-long -A undefined -no-emul-boot -boot-load-size 4 -boot-info-table -b $boot/loader/isolinux.bin -c $boot/boot.catalog -hide $boot/boot.catalog -hide-joliet $boot/boot.catalog -o ../$1 ./ 2> /dev/null
+  cd ..
+}
+
+bootemu ()
+{
+  forp=$PWD/$1
+  mkdir -p $forp
+  rm -f $forp/uptime
+  # path on portia
+  qemu="/usr/local/bin/qemu-system-x86_64"
+  if ! test -x $qemu; then
+     qemu=qemu-kvm
+  else
+     qemu="$qemu -enable-kvm"
+  fi
+  # -hda fat:rw:$forp
+  $qemu -soundhw pcspk -monitor telnet:0.0.0.0:1025,server,nowait -serial file:$forp/trace -cdrom newlive.iso -boot d -m 512 -vnc :70 &
+  qemupid=$!
+  lastsize=0
+  for i in `seq 0 15`; do
+    echo $((i/2)) min $lastsize
+    sleep 30
+    if ! test -s $forp/trace; then continue; fi
+    csize=`stat -c %s $forp/trace`
+    if test $csize == $lastsize; then break; fi  
+    lastsize=$csize
+  done
+  exec 3<>/dev/tcp/localhost/1025
+  echo "screendump default" >&3
+  sleep 1
+  convert default $forp/default.jpg
+  echo "sendkey ctrl-alt-f1" >&3
+  sleep 1
+  echo "screendump default" >&3
+  sleep 1
+  convert default $forp/tty1.jpg
+  echo "sendkey ctrl-alt-f2" >&3
+  sleep 1
+  echo "screendump default" >&3
+  sleep 1
+  convert default $forp/tty2.jpg
+  echo "sendkey ctrl-alt-f3" >&3
+  sleep 1
+  echo "screendump default" >&3
+  sleep 1
+  convert default $forp/tty3.jpg
+  rm default
+  echo "quit" >&3
+  dos2unix $forp/trace
+}
+
+cd=$1
+proj=$2
+status=`curl -s http://buildservice.suse.de:5352/build/$proj/$cd/_status | grep code= | sed -e 's,.*code="\(.*\)".*,\1,'`
+case $status in 
+   finished|succeeded|disabled)
+	;;
+   *)
+	#echo "already rebuilding $cd: $status"
+	exit 0
+	;;
+esac
+nfile=`curl -s http://buildservice.suse.de:5352/build/$proj/$cd/ | grep filename= | grep -v src.rpm | grep -v promo | grep -v infos | cut -d\" -f2`
+if test -f download/$cd/$nfile; then
+  #echo "already have $cd/$nfile"
+  exit 0
+fi
+rm -f newlive.iso
+#rm -rf download/$cd/
+mkdir -p download/$cd/
+outdir=${cd/\//_}
+echo "downloading $proj/$cd/*.rpm"
+rsync --delete -a --exclude=logfile --exclude=*promo* --exclude=*.src.rpm --exclude=*infos* buildservice2.suse.de::opensuse-internal/build/$proj/$cd/ download/$cd/
+isofile=$(ls -1t download/$cd/*.rpm | tail -n 1)
+rpm -qp --qf "%{VERSION}-%{RELEASE}\n" $isofile > $outdir/rpmversion
+rm -rf CD1
+rpm2cpio $isofile | cpio -i
+icfg=$(ls -1 CD1/boot/*/loader/isolinux.cfg)
+cp $icfg $icfg.orig
+sed -i -e "s,preloadlog=/dev/null,,; s,showopts,showopts preloadlog=/dev/ttyS0 vga=0x317," $icfg
+sed -i -e "s,timeout 200,timeout 5," $icfg
+mkiso newlive.iso
+bootemu $outdir
+sed -i -e 's,\*n,,g' $outdir/trace
+
+mv $icfg.orig $icfg
+sed -i -e "s,showopts,showopts cliclog=/dev/ttyS0 vga=0x317," $icfg
+sed -i -e "s,timeout 200,timeout 5," $icfg
+mkiso newlive.iso
+bootemu clic
+mv clic/trace $outdir/clic
+rm -rf clic
+
+rm -rf CD1
+mv newlive.iso $outdir.iso
+
+exit 1
---
+++ get_preloads.sh
@@ -0,0 +1,69 @@
+ret=0
+if ! lockfile -0 -r1 lock 2> /dev/null; then
+   exit 0
+fi
+
+proj=openSUSE:Factory:Live/snapshot
+for cd in kiwi-profiled-livecd-gnome kiwi-profiled-livecd-kde; do
+   status=`curl -s http://buildservice.suse.de:5352/build/$proj/i586/$cd/_status | grep code= | sed -e 's,.*code="\(.*\)".*,\1,'`
+   case $status in
+      finished|succeeded|building|scheduled|dispatching)
+        ;;
+      *)
+	binaries=`curl -s http://buildservice.suse.de:5352/build/$proj/i586/$cd/ | grep filename=`
+	if test -n "$binaries"; then
+	   echo "wiping $cd"
+  	   osc wipebinaries openSUSE:Factory:Live $cd
+           osc wipebinaries openSUSE:Factory:Live promo-dvd-parts
+	fi
+        ;;
+   esac
+done
+
+ulimit -c unlimited
+ret=1
+sh gather_preload.sh x86_64/kiwi-image-livecd-x11 $proj || ret=1
+sh gather_preload.sh i586/kiwi-image-livecd-x11 $proj || ret=1
+sh gather_preload.sh x86_64/kiwi-image-livecd-kde $proj || ret=1
+sh gather_preload.sh i586/kiwi-image-livecd-kde $proj || ret=1
+sh gather_preload.sh x86_64/kiwi-image-livecd-gnome $proj || ret=1
+sh gather_preload.sh i586/kiwi-image-livecd-gnome $proj || ret=1
+if test "$ret" = 0; then 
+  rm -f lock
+  exit 0
+fi
+#cp i586_kiwi-image-livecd-gnome/rpmversion i586_kiwi-image-livecd-x11/rpmversion
+#cp i586_kiwi-image-livecd-gnome/rpmversion x86_64_kiwi-image-livecd-x11/rpmversion
+
+for flavor in x11 gnome kde; do 
+  lines=`cat *-$flavor/rpmversion | sort -u | wc -l`
+  if test "$lines" -gt 1; then
+    echo "$lines different versions"
+    continue
+  fi
+  rpmv=`cat *-$flavor/rpmversion | sort -u`
+  rm -rf openSUSE:Factory:Live 
+  osc checkout openSUSE:Factory:Live/preload-lists-$flavor
+  rm -rf pl
+  mkdir pl
+  for arch in x86_64 i586; do
+    if test -f "$arch"_kiwi-image-livecd-$flavor/trace; then
+      cp "$arch"_kiwi-image-livecd-$flavor/trace pl/livecd-$flavor-$arch || true
+      cp "$arch"_kiwi-image-livecd-$flavor/clic pl/clic-$flavor-$arch || true
+    fi
+  done
+  cd pl
+  tar cvjf ../openSUSE:Factory:Live/preload-lists-$flavor/preload-lists.tar.bz2 .
+  cd ..
+  sed -i -e "s,Provides:.*cliclists.*,Provides: cliclists-$flavor = $rpmv," openSUSE:Factory:Live/preload-lists-$flavor/preload-lists-$flavor.spec
+  rm -rf pl
+  commit=$(git log -n 1 HEAD | head -n 1)
+  osc commit -m "$commit $rpmv" openSUSE:Factory:Live/preload-lists-$flavor
+  rm -rf openSUSE:Factory:Live
+  git commit -a -m "new run $rpmv"
+  git push
+done
+git commit -a -m "new run $rpmv"
+git push
+rm -f lock
+
