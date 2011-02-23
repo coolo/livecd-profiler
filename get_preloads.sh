788f12146df8dd8ffa4813f4421e26750ab10f54
LiveCD Tester <coolo@suse.de>
2011-02-23 10:20:51 +0100

    new run

--- get_preloads.sh
+++ get_preloads.sh
@@ -3,19 +3,20 @@ if ! lockfile -0 -r1 lock 2> /dev/null; then
    exit 0
 fi
 
-proj=openSUSE:Factory:Live/standard
+proj=openSUSE:11.4:Live
+repo=standard
 for cd in kiwi-profiled-livecd-gnome kiwi-profiled-livecd-kde kiwi-image-livecd-gnome kiwi-image-livecd-kde; do
  for arch in i586 x86_64; do 
-   status=`curl -s http://buildservice.suse.de:5352/build/$proj/$arch/$cd.$arch/_status | grep code= | sed -e 's,.*code="\(.*\)".*,\1,'`
+   status=`curl -s http://buildservice.suse.de:5352/build/$proj/$repo/$arch/$cd.$arch/_status | grep code= | sed -e 's,.*code="\(.*\)".*,\1,'`
    case $status in
       finished|succeeded|building|scheduled|dispatching|disabled)
         ;;
       *)
-	binaries=`curl -s http://buildservice.suse.de:5352/build/$proj/$arch/$cd.$arch/ | grep filename=`
+	binaries=`curl -s http://buildservice.suse.de:5352/build/$proj/$repo/$arch/$cd.$arch/ | grep filename=`
 	if test -n "$binaries"; then
 	   echo "wiping $cd"
-  	   osc wipebinaries openSUSE:Factory:Live $cd.$arch
-           osc wipebinaries openSUSE:Factory:Live promo-dvd-parts -a $arch
+  	   osc wipebinaries $proj $cd.$arch
+           osc wipebinaries $proj promo-dvd-parts -a $arch
 	fi
         ;;
    esac
@@ -26,10 +27,11 @@ ulimit -c unlimited
 ret=0
 #sh gather_preload.sh x86_64/kiwi-image-livecd-x11 $proj || ret=1
 #sh gather_preload.sh i586/kiwi-image-livecd-x11 $proj || ret=1
-sh gather_preload.sh x86_64 kiwi-image-livecd-kde $proj || ret=1
-sh gather_preload.sh i586 kiwi-image-livecd-kde $proj || ret=1
-sh gather_preload.sh x86_64 kiwi-image-livecd-gnome $proj || ret=1
-sh gather_preload.sh i586 kiwi-image-livecd-gnome $proj || ret=1
+sh gather_preload.sh x86_64 kiwi-image-livecd-kde $proj/$repo || ret=1
+sh gather_preload.sh i586 kiwi-image-livecd-kde $proj/$repo || ret=1
+sh gather_preload.sh x86_64 kiwi-image-livecd-gnome $proj/$repo || ret=1
+sh gather_preload.sh i586 kiwi-image-livecd-gnome $proj/$repo || ret=1
+
 if test "$ret" = 0; then 
   rm -f lock
   exit 0
@@ -40,15 +42,15 @@ for flavor in gnome kde; do
   for arch in x86_64 i586; do
     rpmv=`cat "$arch"_kiwi-image-livecd-$flavor/rpmversion | sort -u`
     if test -f "$arch"_kiwi-image-livecd-$flavor/trace; then
-      rm -rf openSUSE:Factory:Live
-      osc checkout openSUSE:Factory:Live/preload-lists-$flavor-$arch
-      cp "$arch"_kiwi-image-livecd-$flavor/trace openSUSE:Factory:Live/preload-lists-$flavor-$arch/livecd || true
-      cp "$arch"_kiwi-image-livecd-$flavor/clic openSUSE:Factory:Live/preload-lists-$flavor-$arch/clic || true
-      sed -i -e "s,Provides:.*cliclists.*,Provides: cliclists-$flavor = $rpmv," openSUSE:Factory:Live/preload-lists-$flavor-$arch/preload-lists-$flavor.spec
-      (cd openSUSE:Factory:Live/preload-lists-$flavor-$arch && osc commit -m "$commit $rpmv")
+      rm -rf $proj
+      osc checkout $proj/preload-lists-$flavor-$arch
+      cp "$arch"_kiwi-image-livecd-$flavor/trace $proj/preload-lists-$flavor-$arch/livecd || true
+      cp "$arch"_kiwi-image-livecd-$flavor/clic $proj/preload-lists-$flavor-$arch/clic || true
+      sed -i -e "s,Provides:.*cliclists.*,Provides: cliclists-$flavor = $rpmv," $proj/preload-lists-$flavor-$arch/preload-lists-$flavor.spec
+      (cd $proj/preload-lists-$flavor-$arch && osc commit -m "$commit $rpmv")
     fi
   done
-  rm -rf openSUSE:Factory:Live
+  rm -rf $proj
 done
 git commit -a -m "new run"
 git push
